17eb2c2a9302d0f09fbea2be042bccfd4518fafc
sedx876 <swatkins876@gmail.com>
2020-10-08 07:15:34 -0400

    added todolist compoent

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList';
 import Typography from '@material-ui/core/Typography';
 
 function App() {
+  const [ todos, setTodos ] = useState([])
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
-
-      <TodoForm saveTodo={console.warn} />
+      <TodoForm saveTodo={(todoText) => {
+        const trimmedText = todoText.trim()
+        if (trimmedText.length > 0) {
+          setTodos([...todos, trimmedText])
+        }
+      }} />
+      <TodoList todos={todos} />
     </div>
   );
 }
