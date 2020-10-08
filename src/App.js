dbb855cec10248df19262ffdaba63d5f26bcc18b
sedx876 <swatkins876@gmail.com>
2020-10-08 06:51:39 -0400

    added todoform component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './styles/App.css';
+import TodoForm from './components/TodoForm'
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
-      
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+
+      <TodoForm saveTodo={console.warn} />
     </div>
   );
 }
