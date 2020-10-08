8162fabd80efb2e05089cf01927ac6ae92c28ae7
sedx876 <swatkins876@gmail.com>
2020-10-08 07:42:52 -0400

    added useInputState component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import TextField from '@material-ui/core/TextField';
+import useInputState from './useInputState';
 
 const TodoForm = ({ saveTodo }) => {
-  const [ value, setValue ] = useState('')
+  const { value, reset, onChange } = useInputState('');
+
   return (
-    <form 
+    <form
       onSubmit={(event) => {
-        event.preventDefault()
-        saveTodo(value)
-        setValue('')
-      }}>
-      <TextField
-      variant='outlined'
-      placeholder='Add todo'
-      margin='normal'
-      onChange={(event) => {
-        setValue(event.target.value)
+        event.preventDefault();
+
+        saveTodo(value);
+        reset();
       }}
-      value={value}
+    >
+      <TextField
+        variant="outlined"
+        placeholder="Add todo"
+        margin="normal"
+        onChange={onChange}
+        value={value}
       />
     </form>
   )
